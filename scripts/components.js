157c68875d4c7f3c4208633a39af6bd566cf0aaf
Patricia Sunday <patriciasnsunday@outlook.com>
2026-03-23 16:18:56 -0400

    refactor: slightly resize footer and minor cleanups

--- scripts/components.js
+++ scripts/components.js
@@ -4,8 +4,6 @@ Author: Patricia Sunday
 Purpose: injecting relative nav and footer into each page
 */
 
-
-
 document.addEventListener('DOMContentLoaded', function () {
     // check if current page is in pages folder (e.g. contactme.html) or at proj root (i.e. index.html)
     const isInPages = window.location.pathname.includes('/pages/');
@@ -21,8 +19,8 @@ document.addEventListener('DOMContentLoaded', function () {
     </nav>`;
 
     // set universal footer
-    const footerHTML = `<footer class="flex justify-between py-3 px-8 md:px-12 text-sm bg-[var(--primary-contrast)] text-[var(--bg-color)] border-t-2 border-[var(--subtitle-color)]">
-        <p>Copyright &copy; 2025 Patricia Sunday. All Rights Reserved</p>
+    const footerHTML = `<footer class="flex justify-between items-center py-3 px-8 md:px-12 text-sm bg-[var(--primary-contrast)] text-[var(--bg-color)] border-t-2 border-[var(--subtitle-color)]">
+        <p class="text-base">Copyright &copy; 2025 Patricia Sunday. All Rights Reserved</p>
         <div class="flex gap-5">
             <a href="https://www.linkedin.com/in/patricia-sunday" class="footer-image"><img src="${base}media/linkedin-logo.png" alt="LinkedIn logo"></a>
             <a href="https://github.com/patriciasunday" class="footer-image"><img src="${base}media/github-logo.svg" alt="GitHub logo"></a>
